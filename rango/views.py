db67ce078bbb9d37122fa11b70c5f3eb5a958586
Lukas Knapek <knapek.luk@googlemail.com>
2018-01-14 12:09:54 +0000

    TWD Chapter 3 finished (exercises)

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    response = """Rango says hey there partner! </br>
+                    Perhaps you would like to check the <a href="/rango/about/">About page</a>?"""
+    return HttpResponse(response)
+
+def about(request):
+    response = """Rango says this here is the about page! </br>
+                    There ain't much yet, maybe you're better off going back to the <a href="/rango/">main page</a>?"""
+    return HttpResponse(response)
