3d1d0a7a85ac9a28cb74e9ef539d38af7a1b3518
Lukas Knapek <knapek.luk@googlemail.com>
2018-01-17 19:17:31 +0000

    TWD Chapter 5 finished (exercises)

--- populate_rango.py
+++ populate_rango.py
@@ -21,13 +21,13 @@ def populate():
                     {"title": "Flask", "url": "http://flask.pocoo.org"}]
 
     # Categories dictionary
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages}}
+    cats = {"Python": {"pages": python_pages, "views": 128, "likes": 64},
+            "Django": {"pages": django_pages, "views": 64, "likes": 32},
+            "Other Frameworks": {"pages": other_pages, "views": 32, "likes": 16}}
 
     # Go through the categories dictionary, create the categories and add all of their pages to them
     for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -45,8 +45,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
--- rango/admin.py
+++ rango/admin.py
@@ -6,4 +6,9 @@ from rango.models import Page, Category
 
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
