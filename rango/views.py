d3e9cb31046ffed46ec40d9b53481fda53b03e8b
Lukas Knapek <knapek.luk@googlemail.com>
2018-01-19 11:25:34 +0000

    Implemented Add Category form

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 def index(request):
     # Query the database for all currently stored categories sorted by the number of likes (descending)
@@ -43,3 +44,40 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    # Is the request POST, i.e. has the user submitted the form?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        # We only save the form if it is valid, otherwise
+        # print out errors in terminal
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    # If the request is not POST, it is GET and we display
+    # the appropriate template with the form
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
